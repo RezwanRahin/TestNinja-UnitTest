0d1de9cb3bda512da9bc019226b39705a10e8cb2
rezwan <rezwanshahriorrahin@gmail.com>
2024-03-20 22:00:04 +0600

    modified the name of the TestMethods of 'ReservationTests' class

--- TestNinja.UnitTests/ReservationTests.cs
+++ TestNinja.UnitTests/ReservationTests.cs
@@ -8,7 +8,7 @@ namespace TestNinja.UnitTests
 	public class ReservationTests
 	{
 		[TestMethod]
-		public void CanBeCancelledBy_UserIsAdmin_ReturnsTrue()
+		public void CanBeCancelledBy_AdminCancelling_ReturnsTrue()
 		{
 			// Arrange
 			var reservation = new Reservation();
@@ -21,7 +21,7 @@ namespace TestNinja.UnitTests
 		}
 
 		[TestMethod]
-		public void CanBeCancelledBy_SameUserCancellingTheReservation_ReturnsTrue()
+		public void CanBeCancelledBy_SameUserCancelling_ReturnTrue()
 		{
 			var user = new User();
 			var reservation = new Reservation { MadeBy = user };
@@ -32,7 +32,7 @@ namespace TestNinja.UnitTests
 		}
 
 		[TestMethod]
-		public void CanBeCancelledBy_AnotherUserCancellingReservation_ReturnFalse()
+		public void CanBeCancelledBy_AnotherUserCancelling_ReturnFalse()
 		{
 			var reservation = new Reservation { MadeBy = new User() };
 
