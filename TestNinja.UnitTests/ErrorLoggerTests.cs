ad1228974bb8a2e13486cde8d3a7fde3560187d6
rezwan <rezwanshahriorrahin@gmail.com>
2024-04-12 19:26:25 +0600

    add SetUp method to 'ErrorLoggerTests' class

--- TestNinja.UnitTests/ErrorLoggerTests.cs
+++ TestNinja.UnitTests/ErrorLoggerTests.cs
@@ -6,6 +6,14 @@ namespace TestNinja.UnitTests
 	[TestFixture]
 	public class ErrorLoggerTests
 	{
+		private ErrorLogger _logger;
+
+		[SetUp]
+		public void SetUp()
+		{
+			_logger = new ErrorLogger();
+		}
+
 		[Test]
 		public void Log_WhenCalled_SetTheLastErrorProperty()
 		{
