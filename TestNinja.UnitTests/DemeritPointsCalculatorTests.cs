cfe70f02170ddfb3d3e93894479a492a96ec0c5b
rezwan <rezwanshahriorrahin@gmail.com>
2024-04-14 19:53:25 +0600

    add SetUp method to 'DemeritPointsCalculatorTests' class

--- TestNinja.UnitTests/DemeritPointsCalculatorTests.cs
+++ TestNinja.UnitTests/DemeritPointsCalculatorTests.cs
@@ -1,9 +1,17 @@
 ﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
 
 namespace TestNinja.UnitTests
 {
 	[TestFixture]
 	public class DemeritPointsCalculatorTests
 	{
+		private DemeritPointsCalculator _calculator;
+
+		[SetUp]
+		public void SetUp()
+		{
+			_calculator = new DemeritPointsCalculator();
+		}
 	}
 }
