cecb9dad73166d1ec2fa3076b05ae80015ea4323
rezwan <rezwanshahriorrahin@gmail.com>
2024-04-14 19:54:15 +0600

    add a test method as 'CalculateDemeritPoints_SpeedISOutOfRange_ThrowArgumentOutOfRangeOfRangeArgument' to 'DemeritPointsCalculatorTests' class

--- TestNinja.UnitTests/DemeritPointsCalculatorTests.cs
+++ TestNinja.UnitTests/DemeritPointsCalculatorTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NUnit.Framework;
 using TestNinja.Fundamentals;
 
 namespace TestNinja.UnitTests
@@ -12,6 +13,14 @@ namespace TestNinja.UnitTests
 		public void SetUp()
 		{
 			_calculator = new DemeritPointsCalculator();
+		}
+
+		[Test]
+		[TestCase(-1)]
+		[TestCase(301)]
+		public void CalculateDemeritPoints_SpeedIsOutOfRange_ThrowArgumentOutOfRangeOfRangeArgument(int speed)
+		{
+			Assert.That(() => _calculator.CalculateDemeritPoints(speed), Throws.Exception.TypeOf<ArgumentOutOfRangeException>());
 		}
 	}
 }
