6e3d6696c16476b200cf501d899de07137dfbbc5
rezwan <rezwanshahriorrahin@gmail.com>
2024-04-14 19:56:06 +0600

    add a test method as 'CalculateDemeritPoints_WhenCalled_ReturnDemeritPoints' to 'DemeritPointsCalculatorTests' class

--- TestNinja.UnitTests/DemeritPointsCalculatorTests.cs
+++ TestNinja.UnitTests/DemeritPointsCalculatorTests.cs
@@ -22,5 +22,19 @@ namespace TestNinja.UnitTests
 		{
 			Assert.That(() => _calculator.CalculateDemeritPoints(speed), Throws.Exception.TypeOf<ArgumentOutOfRangeException>());
 		}
+
+		[Test]
+		[TestCase(0, 0)]
+		[TestCase(64, 0)]
+		[TestCase(65, 0)]
+		[TestCase(66, 0)]
+		[TestCase(70, 1)]
+		[TestCase(75, 2)]
+		public void CalculateDemeritPoints_WhenCalled_ReturnDemeritPoints(int speed, int expectedResult)
+		{
+			var points = _calculator.CalculateDemeritPoints(speed);
+
+			Assert.That(points, Is.EqualTo(expectedResult));
+		}
 	}
 }
