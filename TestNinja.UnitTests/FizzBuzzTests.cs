42e17ff7d2c19a92869c2638a77878295efc872a
rezwan <rezwanshahriorrahin@gmail.com>
2024-04-14 15:46:07 +0600

    add a test method as 'GetOutput_InputIsDivisibleBy3And5_ReturnFizzBuzz' to 'FizzBuzzTests' class

--- TestNinja.UnitTests/FizzBuzzTests.cs
+++ TestNinja.UnitTests/FizzBuzzTests.cs
@@ -1,9 +1,17 @@
 ﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
 
 namespace TestNinja.UnitTests
 {
 	[TestFixture]
 	public class FizzBuzzTests
 	{
+		[Test]
+		public void GetOutput_InputIsDivisibleBy3And5_ReturnFizzBuzz()
+		{
+			var result = FizzBuzz.GetOutput(15);
+
+			Assert.That(result, Is.EqualTo("FizzBuzz"));
+		}
 	}
 }
