14134c37872d299147bfdf9567bc09ccc498b08c
rezwan <rezwanshahriorrahin@gmail.com>
2024-04-15 15:59:39 +0600

    add a test method as 'Pop_NonEmptyStack_ReturnTopItem' to 'StackTests' class

--- TestNinja.UnitTests/StackTests.cs
+++ TestNinja.UnitTests/StackTests.cs
@@ -33,5 +33,26 @@ namespace TestNinja.UnitTests
 
 			Assert.That(_stack.Count, Is.EqualTo(1));
 		}
+
+		[Test]
+		public void Pop_EmptyStack_ThrowInvalidOperationException()
+		{
+			Assert.That(() => _stack.Pop(), Throws.InvalidOperationException);
+		}
+
+		[Test]
+		public void Pop_NonEmptyStack_ReturnTopItem()
+		{
+			// Arrange
+			_stack.Push("a");
+			_stack.Push("b");
+			_stack.Push("c");
+
+			// Act
+			var result = _stack.Pop();
+
+			// Assert
+			Assert.That(result, Is.EqualTo("c"));
+		}
 	}
 }
