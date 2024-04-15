19504ef122d34492881d08f33dbfd4a33fea5ae8
rezwan <rezwanshahriorrahin@gmail.com>
2024-04-15 15:57:07 +0600

    add SetUp method to 'StackTests' class

--- TestNinja.UnitTests/StackTests.cs
+++ TestNinja.UnitTests/StackTests.cs
@@ -1,9 +1,17 @@
 ﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
 
 namespace TestNinja.UnitTests
 {
 	[TestFixture]
 	public class StackTests
 	{
+		private Stack<string> _stack;
+
+		[SetUp]
+		public void SetUp()
+		{
+			_stack = new Stack<string>();
+		}
 	}
 }
