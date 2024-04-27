f7fd998a4585daca23c60f8368a2e602f9978680
rezwan <rezwanshahriorrahin@gmail.com>
2024-04-27 20:23:15 +0600

    add a method as 'GetUnprocessedVideosAsCsv' to 'VideoRepository' class

--- TestNinja/Mocking/VideoRepository.cs
+++ TestNinja/Mocking/VideoRepository.cs
@@ -1,6 +1,21 @@
-﻿namespace TestNinja.Mocking
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace TestNinja.Mocking
 {
 	public class VideoRepository
 	{
+		public IEnumerable<Video> GetUnprocessedVideosAsCsv()
+		{
+			using (var context = new VideoContext())
+			{
+				var videos =
+					(from video in context.Videos
+					 where !video.IsProcessed
+					 select video).ToList();
+
+				return videos;
+			}
+		}
 	}
 }
