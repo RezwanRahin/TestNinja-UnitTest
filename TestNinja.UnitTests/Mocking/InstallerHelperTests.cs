69e006c89eddf1a4c3775bd915845d8821622a1e
rezwan <rezwanshahriorrahin@gmail.com>
2024-05-02 19:37:10 +0600

    add SetUp method to 'InstallerHelper' class
    - introduction of 2 private fields of type Mock<IFileDownloader> & InstallerHelper

--- TestNinja.UnitTests/Mocking/InstallerHelperTests.cs
+++ TestNinja.UnitTests/Mocking/InstallerHelperTests.cs
@@ -1,9 +1,20 @@
-﻿using NUnit.Framework;
+﻿using Moq;
+using NUnit.Framework;
+using TestNinja.Mocking;
 
 namespace TestNinja.UnitTests.Mocking
 {
 	[TestFixture]
 	public class InstallerHelperTests
 	{
+		private Mock<IFileDownloader> _fileDownloader;
+		private InstallerHelper _installerHelper;
+
+		[SetUp]
+		public void SetUp()
+		{
+			_fileDownloader = new Mock<IFileDownloader>();
+			_installerHelper = new InstallerHelper(_fileDownloader.Object);
+		}
 	}
 }
