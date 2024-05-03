cd652b4296beb9c554b3e4607db193137a5a5732
rezwan <rezwanshahriorrahin@gmail.com>
2024-05-03 19:55:12 +0600

    add a method as 'DeleteEmployee' to 'EmployeeStorage' class

--- TestNinja/Mocking/EmployeeStorage.cs
+++ TestNinja/Mocking/EmployeeStorage.cs
@@ -8,5 +8,13 @@
 		{
 			_db = new EmployeeContext();
 		}
+
+		public void DeleteEmployee(int id)
+		{
+			var employee = _db.Employees.Find(id);
+			if (employee == null) return;
+			_db.Employees.Remove(employee);
+			_db.SaveChanges();
+		}
 	}
 }
