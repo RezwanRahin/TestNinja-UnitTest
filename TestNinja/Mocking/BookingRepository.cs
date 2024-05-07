a68f9215f99e901344f37f72fcc932a5b0019141
rezwan <rezwanshahriorrahin@gmail.com>
2024-05-07 20:08:01 +0600

    add a method as 'GetActiveBookings' to 'BookingRepository' class

--- TestNinja/Mocking/BookingRepository.cs
+++ TestNinja/Mocking/BookingRepository.cs
@@ -1,6 +1,18 @@
-﻿namespace TestNinja.Mocking
+﻿using System.Linq;
+
+namespace TestNinja.Mocking
 {
 	public class BookingRepository
 	{
+		public IQueryable<Booking> GetActiveBookings(int? excludedBookingId = null)
+		{
+			var unitOfWork = new UnitOfWork();
+			var bookings = unitOfWork.Query<Booking>().Where(b => b.Status != "Cancelled");
+
+			if (excludedBookingId.HasValue)
+				bookings = bookings.Where(b => b.Id != excludedBookingId.Value);
+
+			return bookings;
+		}
 	}
 }
