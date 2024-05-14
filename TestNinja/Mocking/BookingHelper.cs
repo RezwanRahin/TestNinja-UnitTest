b8cb77f04fdc0d1dac4491362f82a444786aeac4
rezwan <rezwanshahriorrahin@gmail.com>
2024-05-14 19:24:21 +0600

    refactor: extract an interface as 'IUnitOfWork' in 'Mocking/BookingHelper' class of production project

--- TestNinja/Mocking/BookingHelper.cs
+++ TestNinja/Mocking/BookingHelper.cs
@@ -20,13 +20,18 @@ namespace TestNinja.Mocking
         }
     }
 
-    public class UnitOfWork
-    {
-        public IQueryable<T> Query<T>()
-        {
-            return new List<T>().AsQueryable();
-        }
-    }
+	public interface IUnitOfWork
+	{
+		IQueryable<T> Query<T>();
+	}
+
+	public class UnitOfWork : IUnitOfWork
+	{
+		public IQueryable<T> Query<T>()
+		{
+			return new List<T>().AsQueryable();
+		}
+	}
 
     public class Booking
     {
