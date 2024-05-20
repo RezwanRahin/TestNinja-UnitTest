64f9d2de9eddf489e1663be512be3f17dd2f6a38
rezwan <rezwanshahriorrahin@gmail.com>
2024-05-20 14:31:19 +0600

    refactor: utilize 'VerifyEmailNotSent' private method in 'SendStatementEmails_StatementFileNameIsWhitespace_ShouldNotEmailTheStatement' test method of 'HouseKeeperServiceTests' class

--- TestNinja.UnitTests/Mocking/HouseKeeperServiceTests.cs
+++ TestNinja.UnitTests/Mocking/HouseKeeperServiceTests.cs
@@ -130,7 +130,7 @@ namespace TestNinja.UnitTests.Mocking
 
 			_service.SendStatementEmails(_statementDate);
 
-			_emailSender.Verify(e => e.EmailFile(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()), Times.Never);
+			VerifyEmailNotSent();
 		}
 
 		private void VerifyEmailSent()
