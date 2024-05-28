84a58a028c04801fdf5bb0137bac317bc4b088f3
rezwan <rezwanshahriorrahin@gmail.com>
2024-05-28 20:06:20 +0600

    remove 'SaveStatement' method from 'HousekeeperHelper' class of production project

--- TestNinja/Mocking/HousekeeperHelper.cs
+++ TestNinja/Mocking/HousekeeperHelper.cs
@@ -49,24 +49,6 @@ namespace TestNinja.Mocking
             return true;
         }
 
-        private static string SaveStatement(int housekeeperOid, string housekeeperName, DateTime statementDate)
-        {
-            var report = new HousekeeperStatementReport(housekeeperOid, statementDate);
-
-            if (!report.HasData)
-                return string.Empty;
-
-            report.CreateDocument();
-
-            var filename = Path.Combine(
-                Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),
-                string.Format("Sandpiper Statement {0:yyyy-MM} {1}.pdf", statementDate, housekeeperName));
-
-            report.ExportToPdf(filename);
-
-            return filename;
-        }
-
         private static void EmailFile(string emailAddress, string emailBody, string filename, string subject)
         {
             var client = new SmtpClient(SystemSettingsHelper.EmailSmtpHost)
