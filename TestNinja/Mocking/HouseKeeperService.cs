c2403d496344c3b74182cb90993da0b3af04d2a0
rezwan <rezwanshahriorrahin@gmail.com>
2024-06-10 14:40:36 +0600

    refactor: rename 'HousekeeperHelper' class to 'HouseKeeperService'

--- TestNinja/Mocking/HouseKeeperService.cs
+++ TestNinja/Mocking/HouseKeeperService.cs
@@ -2,14 +2,14 @@ using System;
 
 namespace TestNinja.Mocking
 {
-    public class HousekeeperHelper
+    public class HouseKeeperService
     {
         private readonly IUnitOfWork _unitOfWork;
         private readonly IStatementGenerator _statementGenerator;
         private readonly IEmailSender _emailSender;
         private readonly IXtraMessageBox _messageBox;
 
-        public HousekeeperHelper(IUnitOfWork unitOfWork,
+        public HouseKeeperService(IUnitOfWork unitOfWork,
             IStatementGenerator statementGenerator,
             IEmailSender emailSender,
             IXtraMessageBox messageBox)
